fe341d3e568467c954b60753536dfd9fef0febb6
Eric Dallo <ericdallo06@hotmail.com>
2021-11-24 16:27:31 -0300

    Fix deploy

--- scripts/lein_clj_kondo/ci.clj
+++ scripts/lein_clj_kondo/ci.clj
@@ -17,10 +17,10 @@
                    (format "com.github.clj-kondo/lein-clj-kondo \"%s\"" tag))
   (replace-in-file "CHANGELOG.md"
                    #"## Unreleased"
-                   (format "## Unreleased\n\n## %s" tag))
+                   (format "## Unreleased\n\n## v%s" tag))
   (shell "git add project.clj CHANGELOG.md")
   (shell (format "git commit -m \"Release: %s\"" tag))
-  (shell (str "git tag " tag))
+  (shell (str "git tag v" tag))
   (shell "git push origin HEAD")
   (shell "git push origin --tags"))
 
