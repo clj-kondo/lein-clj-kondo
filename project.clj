fa3d8a8e719c47015e2d5c4ddc180f61c78dd1ca
Eric Dallo <ericdallo06@hotmail.com>
2022-05-04 13:08:50 -0300

    Bump clj-kondo to 2022.04.25

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
   :pedantic? :warn
-  :dependencies [[clj-kondo/clj-kondo "2021.10.19"]]
+  :dependencies [[clj-kondo/clj-kondo "2022.04.25"]]
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_user
                                     :password :env/clojars_pass
