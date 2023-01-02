985b5998703488119ba255fe2f67cdb1d2a2a5d6
Michiel Borkent <michielborkent@gmail.com>
2023-01-02 16:09:55 +0100

    Fix tagging

--- scripts/lein_clj_kondo/ci.clj
+++ scripts/lein_clj_kondo/ci.clj
@@ -1,7 +1,8 @@
 (ns lein-clj-kondo.ci
   (:require
    [babashka.tasks :refer [shell]]
-   [clojure.string :as string]))
+   [clojure.string :as string]
+   [clojure.string :as str]))
 
 (defn ^:private replace-in-file [file regex content]
   (as-> (slurp file) $
@@ -10,19 +11,20 @@
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn tag [& [tag]]
-  (shell "git fetch origin")
-  (shell "git pull origin HEAD")
-  (replace-in-file "project.clj"
-                   #"com.github.clj-kondo/lein-clj-kondo \"[0-9]+.[0-9]+.[0-9]+.*\""
-                   (format "com.github.clj-kondo/lein-clj-kondo \"%s\"" tag))
-  (replace-in-file "CHANGELOG.md"
-                   #"## Unreleased"
-                   (format "## Unreleased\n\n## v%s" tag))
-  (shell "git add project.clj CHANGELOG.md")
-  (shell (format "git commit -m \"Release: %s\"" tag))
-  (shell (str "git tag v" tag))
-  (shell "git push origin HEAD")
-  (shell "git push origin --tags"))
+  (let [tag (str/replace tag #"^v" "")]
+    (shell "git fetch origin")
+    (shell "git pull origin HEAD")
+    (replace-in-file "project.clj"
+                     #"com.github.clj-kondo/lein-clj-kondo \"[0-9]+.[0-9]+.[0-9]+.*\""
+                     (format "com.github.clj-kondo/lein-clj-kondo \"%s\"" tag))
+    (replace-in-file "CHANGELOG.md"
+                     #"## Unreleased"
+                     (format "## Unreleased\n\n## v%s" tag))
+    (shell "git add project.clj CHANGELOG.md")
+    (shell (format "git commit -m \"Release: %s\"" tag))
+    (shell (str "git tag v" tag))
+    (shell "git push origin HEAD")
+    (shell "git push origin --tags")))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn deploy [& _args]
