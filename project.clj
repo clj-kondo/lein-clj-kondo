373216b7761ac4f7c85b30b3b604d36220d261e5
Michiel Borkent <michielborkent@gmail.com>
2024-09-26 16:17:16 +0200

    v2024.09.26

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject com.github.clj-kondo/lein-clj-kondo "2024.08.29"
+(defproject com.github.clj-kondo/lein-clj-kondo "2024.09.26"
   :description "Lein plugin to run clj-kondo"
   :url "https://clj-kondo.github.io/clj-kondo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
   :pedantic? :warn
-  :dependencies [[clj-kondo/clj-kondo "2024.08.29"]]
+  :dependencies [[clj-kondo/clj-kondo "2024.09.26"]]
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_username
                                     :password :env/clojars_password
