202bf569392bc185542bdb3c6259f03ff23433ed
Enzzo <souenzzo@gmail.com>
2025-06-02 09:25:55 -0300

    Update project URL
    this metadata is used in `HomePage`
    Without this change, it is hard to find the project from the artifact.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.github.clj-kondo/lein-clj-kondo "2025.04.07"
   :description "Lein plugin to run clj-kondo"
-  :url "https://clj-kondo.github.io/clj-kondo"
+  :url "https://github.com/clj-kondo/lein-clj-kondo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
