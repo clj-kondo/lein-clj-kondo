6843cb94f14e5b870a9b63e5e0bd4278c265a864
Michiel Borkent <michielborkent@gmail.com>
2025-07-28 15:49:44 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject com.github.clj-kondo/lein-clj-kondo "2025.07.26-2"
+(defproject com.github.clj-kondo/lein-clj-kondo "2025.07.28"
   :description "Lein plugin to run clj-kondo"
   :url "https://github.com/clj-kondo/lein-clj-kondo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
   :pedantic? :warn
-  :dependencies [[clj-kondo/clj-kondo "2025.07.26"]]
+  :dependencies [[clj-kondo/clj-kondo "2025.07.28"]]
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_username
                                     :password :env/clojars_password
